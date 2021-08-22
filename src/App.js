09f88f16eec2091e011c24de2e379255fd63872b
Ganesh Patil <ganeshpatil0101@gmail.com>
2021-08-22 11:54:17 +0530

    lazy load implementation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import About from "./pages/About";
-import Home from "./pages/Home";
-import AddMandal from "./pages/AddMandal";
-import AddGanapati from "./pages/AddGanpati";
 import { makeStyles } from '@material-ui/core/styles';
 import { getCurrentYear } from './components/Handlers';
+const About = lazy(() => import("./pages/About"));
+const Home = lazy(() => import("./pages/Home"));
+const AddMandal = lazy(() => import("./pages/AddMandal"));
+const AddGanapati = lazy(() => import("./pages/AddGanpati"));
 const useStyles = makeStyles(theme => ({
   offset: theme.mixins.toolbar,
 }))
@@ -16,33 +16,34 @@ function App() {
   const classes = useStyles();
   const [year, setYear] = React.useState(getCurrentYear());
   const onSelectYear = (year) => {
-    console.log('======> ', year);
     setYear(year);
   }
   return (
     <Router>
-      <Navbar onSeletedYear={onSelectYear} />
-      <div className={classes.offset}>
-        {/* <header className="App-header"> */}
-          <Switch>
-          <Route exact path="/">
-            <Home currentYear={year} />
-          </Route>
-          <Route exact path="/home">
-            <Home />
-          </Route>
-          <Route path="/about" >
-            <About/>
-          </Route>
-          <Route path="/addmandal" >
-            <AddMandal/>
-          </Route>
-          <Route path="/addganpati" >
-            <AddGanapati/>
-          </Route>
-          </Switch>
-        {/* </header> */}
-      </div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Navbar onSeletedYear={onSelectYear} />
+        <div className={classes.offset}>
+          {/* <header className="App-header"> */}
+            <Switch>
+            <Route exact path="/">
+              <Home currentYear={year} />
+            </Route>
+            <Route exact path="/home">
+              <Home />
+            </Route>
+            <Route path="/about" >
+              <About/>
+            </Route>
+            <Route path="/addmandal" >
+              <AddMandal/>
+            </Route>
+            <Route path="/addganpati" >
+              <AddGanapati/>
+            </Route>
+            </Switch>
+          {/* </header> */}
+        </div>
+      </Suspense>
     </Router>
   );
 }
